390186fe0e806c770de154c9164d7d082f259f85
Carlos Eduardo <cedut.dev@gmail.com>
2025-04-18 19:43:06 -0300

    feat: Adicionado a revisão sobre void

---
+++ typescript/zero-to-hero-course/void-26.ts
@@ -0,0 +1,9 @@
+// Tipo Void
+// Vídeo relacionado: https://www.youtube.com/watch?v=Aguo_gIIh7o&list=PLb2HQ45KP0Wsk-p_0c6ImqBAEFEY-LU9H&index=27
+
+// Uso do void em funções
+function logError(message: string): void {
+  console.error(`Error: ${message}`);
+}
+
+logError("Erro na linha 10");
