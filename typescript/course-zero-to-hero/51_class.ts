afb3d3d30edaa68fe2e667e3a4e7f499dc8d2dd1
Carlos Eduardo <cedut.dev@gmail.com>
2025-04-30 15:43:22 -0300

    feat: Adicionando exemplo básico do uso de Classes - protected

--- typescript/course-zero-to-hero/51_class.ts
+++ typescript/course-zero-to-hero/51_class.ts
@@ -19,3 +19,39 @@ class Pessoa {
 
 const p1 = new Pessoa("carlos", "Eduardo");
 console.log(p1.nomeCompleto());
+
+// Exemplo 02 - protected
+class Estudante {
+  codigo: number;
+  protected nome: string;
+
+  constructor(codigo: number, nome: string) {
+    this.codigo = codigo;
+    this.nome = nome;
+  }
+}
+
+class EstudanteMaisCurso extends Estudante {
+  private curso;
+
+  constructor(codigo: number, nome: string, curso: string) {
+    super(codigo, nome);
+    this.curso = curso;
+  }
+
+  verDados(): string {
+    return `
+      Codigo: ${this.codigo}
+      Nome: ${this.nome}
+      Curso: ${this.curso}
+      `;
+  }
+}
+
+const estudante = new EstudanteMaisCurso(
+  11221,
+  "Carlos",
+  "Desenvolvimento Front-end"
+);
+
+console.log(estudante.verDados());
